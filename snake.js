bbd6d2ce5ae5bea3c84aa535598f01be6adab3be
worldo <worldoooo525@gmail.com>
2026-08-31 13:19:52 +0900

    Add files via upload

--- snake.js
+++ snake.js
@@ -1,12 +1,6 @@
 const gameCanvas = document.getElementById("gameCanvas");
 const ctx = gameCanvas.getContext("2d");
 
-ctx.fillStyle = "white";
-ctx.strokeStyle = "black";
-
-ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
-ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
-
 let snake = [
     { x: 150, y: 150 },
     { x: 140, y: 150 },
@@ -36,5 +30,12 @@ function advanceSnake() {
     snake.unshift(head);
     snake.pop();
 }
+function clearCanvas() {
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "black";
+    ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+    ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+}
+clearCanvas();
 advanceSnake();
 drawSnake();
