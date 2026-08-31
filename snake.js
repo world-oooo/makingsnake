7d686ebec6d2557581c2f9fb2bb0973f727249d7
worldo <worldoooo525@gmail.com>
2026-08-31 12:52:23 +0900

    Add files via upload

--- snake.js
+++ snake.js
@@ -6,3 +6,11 @@ ctx.strokeStyle = "black";
 
 ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
 ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+let snake = [
+    { x: 150, y: 150 },
+    { x: 140, y: 150 },
+    { x: 130, y: 150 },
+    { x: 120, y: 150 },
+    { x: 110, y: 150 }
+];
