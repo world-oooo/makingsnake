25d1485c9c939dca6f77d3f00f2e9e2f834d1ca0
worldo <worldoooo525@gmail.com>
2026-08-31 10:34:32 +0900

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,6 @@
+console.log("Hello, Snake Game!");
+document.getElementById("gameCanvas").width = 400;
+document.getElementById("gameCanvas").height = 400;
+getcanvas = document.getElementById("gameCanvas");
+getcanvas.getContext("2d").fillStyle.fillRect(0, 0, 400, 400);
+getcanvas.getContext("2d").strokeStyle.strokeRect(0, 0, 400, 400);
